de288c01161e1e81beb495d879710ade5de98ebc
natashac693 <natashachar66@gmail.com>
2025-03-25 16:56:12 +0700

    update

--- SeeFood/ContentView.swift
+++ SeeFood/ContentView.swift
@@ -24,7 +24,6 @@ struct ContentView: View {
         NavigationStack {
             
             ZStack{
-            
                 Image("tes")
                     .resizable()
                     .ignoresSafeArea(.all)
