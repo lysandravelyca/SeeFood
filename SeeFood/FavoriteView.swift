917983b2613acac1109b2f20017052f491fbf0b4
natashac693 <natashachar66@gmail.com>
2025-03-27 14:36:19 +0700

    changes in favoriteview and tenantdetailview, added background image for both pages and image for kasturi logo

--- SeeFood/FavoriteView.swift
+++ SeeFood/FavoriteView.swift
@@ -8,80 +8,120 @@ struct FavoriteMenuView: View {
     @State private var selectedMenu: MenuItem?
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Favorite Menus")
-                .font(.title)
-                .bold()
-                .padding()
+        ZStack {
+            // Background Image
+            Image("favoritemenuviewbg") // Replace with your actual image name
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .padding(.bottom, -200)
+            
+            VStack {
+               
 
-            if favoriteManager.favoriteMenus.isEmpty {
-                Text("No favorite menus yet.")
-                    .foregroundColor(.gray)
-                    .italic()
-                    .padding()
-            } else {
-                // Mengelompokkan menu favorit berdasarkan tenantID
-                let groupedMenus = Dictionary(grouping: favoriteManager.favoriteMenus, by: { $0.tenantID })
-                
-                List {
-                    ForEach(groupedMenus.keys.sorted(), id: \.self) { tenantID in
-                        if let tenant = tenants.first(where: { $0.id == tenantID }) {
-                            // Menampilkan Nama Tenant hanya sekali d
-                            Text(tenant.name)
-                                .font(.headline)
-                                .padding(.vertical, 5)
+                if favoriteManager.favoriteMenus.isEmpty {
+                    VStack {
+                        Text("Favorite Menus")
+                            .font(.title)
+                            .bold()
+                            
+                        Text("No favorite menus yet.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                            .padding(.bottom ,20)
+                    }
+                    
+                } else {
+                    // Mengelompokkan menu favorit berdasarkan tenantID
+                    let groupedMenus = Dictionary(grouping: favoriteManager.favoriteMenus, by: { $0.tenantID })
+                    
+                    VStack {
+                        VStack(alignment: .leading) {
+                            Text("Favorite Menus")
+                                .font(.title)
+                                .bold()
+                            
+                            Text("The following menu is sorted by distance closest to you")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.gray)
+                                .padding(.bottom, 20)
                         }
+                        .frame(maxWidth: .infinity, alignment: .leading) // Ensures it stays within screen bounds
+                        .padding(.horizontal, 50) // Optional: Adds some spacing from edges
+                        .padding(.top, 50)
                         
-                        // Menampilkan menu-menu dari tenant
-                        ForEach(groupedMenus[tenantID] ?? [], id: \.menuId) { menu in
-                            HStack(spacing: 15) {
-                                Image(menu.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80, height: 60)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-
-                                VStack(alignment: .leading) {
-                                    Text(menu.name)
+                        List {
+                            ForEach(groupedMenus.keys.sorted(), id: \.self) { tenantID in
+                                if let tenant = tenants.first(where: { $0.id == tenantID }) {
+                                    // Menampilkan Nama Tenant hanya sekali d
+                                    Text(tenant.name)
                                         .font(.headline)
-                                    Text(menu.des)
-                                        .font(.subheadline)
-                                        .foregroundColor(.gray)
-                                    Text("Harga: \(menu.price)")
-                                        .font(.subheadline)
-                                        .bold()
+                                        .padding(.vertical, 5)
                                 }
-
-                                Spacer()
                                 
-                                Button(action: {
-                                    selectedMenu = menu
-                                    showAlert = true
-                                }) {
-                                    Image(systemName: "trash")
-                                        .foregroundColor(.red)
-                                }
-                                .alert("Hapus Favorit", isPresented: $showAlert) {
-                                    Button("Hapus", role: .destructive) {
-                                        if let menuToDelete = selectedMenu {
-                                            favoriteManager.removeFavorite(menu: menuToDelete)
+                                // Menampilkan menu-menu dari tenant
+                                ForEach(groupedMenus[tenantID] ?? [], id: \.menuId) { menu in
+                                    HStack(spacing: 15) {
+                                        Image(menu.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 80, height: 60)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                                        VStack(alignment: .leading) {
+                                            Text(menu.name)
+                                                .font(.headline)
+                                            Text(menu.des)
+                                                .font(.subheadline)
+                                                .foregroundColor(.gray)
+                                            Text("Harga: \(menu.price)")
+                                                .font(.subheadline)
+                                                .bold()
+                                        }
+
+                                        Spacer()
+                                        
+                                        Button(action: {
+                                            selectedMenu = menu
+                                            showAlert = true
+                                        }) {
+                                            Image(systemName: "trash")
+                                                .foregroundColor(.red)
+                                        }
+                                        .alert("Hapus Favorit", isPresented: $showAlert) {
+                                            Button("Hapus", role: .destructive) {
+                                                if let menuToDelete = selectedMenu {
+                                                    favoriteManager.removeFavorite(menu: menuToDelete)
+                                                }
+                                            }
+                                            
+                                            Button("Batal", role: .cancel) {}
+                                        } message: {
+                                            Text("Apakah Anda yakin ingin menghapus menu ini dari favorit?")
                                         }
                                     }
-                                    
-                                    Button("Batal", role: .cancel) {}
-                                } message: {
-                                    Text("Apakah Anda yakin ingin menghapus menu ini dari favorit?")
+                                    .padding(.vertical, 5)
                                 }
                             }
-                            .padding(.vertical, 5)
                         }
+                        .listStyle(.plain)
+                        .scrollContentBackground(.hidden) // Removes extra background spacing
+                        .padding (.horizontal, 40)
                     }
                 }
             }
         }
+        
+        
     }
 }
 
 #Preview {
-    FavoriteMenuView()
+//    let previewManager = FavoriteManager.shared
+//
+//    // Use addFavorite to inject data
+//    previewManager.addFavorite(menu: MenuItem(image: "sample_food_1", name: "Nasi Goreng Spesial", des: "Nasi goreng dengan ayam, udang, dan telur.", price: "25.000", tenantID: UUID()))
+//    
+//    previewManager.addFavorite(menu: MenuItem(image: "sample_food_2", name: "Mie Ayam Bakso", des: "Mie ayam dengan tambahan bakso sapi.", price: "20.000", tenantID: UUID()))
+    return FavoriteMenuView()
 }
--- SeeFood/TenantDetailView.swift
+++ SeeFood/TenantDetailView.swift
@@ -1,156 +1,210 @@
 import SwiftUI
 
 struct TenantDetailView: View {
+    
+    
     let tenant: Tenant
     @ObservedObject var favoriteManager = FavoriteManager.shared
-//    @State private var showAlert = false
+    //    @State private var showAlert = false
     @State private var alertMessage: String = ""
     @State private var showRedirectAlert = false
     @State private var isRedirecting = false
-
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    HStack {
-                        // Logo Tenant
-                        Image(tenant.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 130, height: 100)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
+            ZStack {
+                // Background Image
+                Image("tenantdetailviewbg") // Replace with your actual image name
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .padding(.bottom, -200)
+                
+                ScrollView {
+                    VStack {
+                        // Nama Tenant
+                        Text(tenant.name)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("GOP 9")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.gray)
+                            .multilineTextAlignment(.center)
+                        
+                    }
+                    VStack(alignment: .leading, spacing: 5) {
+                        
                         
                         VStack(alignment: .leading, spacing: 5) {
-                            // Nama Tenant
-                            Text(tenant.name)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .padding(.top)
                             
-                            // Jam Operasional dan Kontak
-                            Text(tenant.hour)
-                            Text(tenant.phone)
+                            // Logo Tenant
+                            Image("tenantkasturi")
+                                .resizable()
+                                .scaledToFill() // Ensures the image fills the frame without distortion
+                                .frame(width: 353, height: 172) // Defines the exact size
+                                .clipShape(RoundedRectangle(cornerRadius: 10)) // Clips the image
+                                .overlay( // Adds a gray border
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.gray, lineWidth: 2))
+                                .clipped() // Ensures anything outside the frame is hidden
+                            
+                            Spacer()
+                            
+                            // Jam Operasional, Kontak, dan Harga
+                            HStack {
+                                Image(systemName: "clock")
+                                    .font(.system(size: 20))
+                                    .foregroundStyle(.green)
+                                    .frame(width: 30, height: 30)
+                                Text(tenant.hour)
+                                    .fontWeight(.semibold)
+                            }
+                            
+                            HStack {
+                                Image(systemName: "phone.fill")
+                                    .font(.system(size: 20))
+                                    .foregroundStyle(.green)
+                                    .frame(width: 30, height: 30)
+                                Text(tenant.phone)
+                                    .fontWeight(.semibold)
+                            }
+                            
+                            HStack {
+                                Image(systemName: "dollarsign")
+                                    .font(.system(size: 20))
+                                    .foregroundStyle(.green)
+                                    .frame(width: 30, height: 30)
+                                Text("Rp15.000 - Rp25.000")
+                                    .fontWeight(.semibold)
+                            }
+                            
+                            Spacer()
                         }
-                    }
-                    
-                    // Daftar Menu
-                    if tenant.menuItems.isEmpty {
-                        Text("Tidak ada menu tersedia")
-                            .foregroundColor(.gray)
-                            .italic()
-                    } else {
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text("Menu:")
-                                .font(.headline)
-                                .padding(.bottom, 5)
-                            
-                            // Iterasi menu
-                            ForEach(tenant.menuItems.indices, id: \.self) { index in
-                                HStack(spacing: 15) {
-                                    // Gambar Menu
-                                    Image(tenant.menuItems[index].image)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 130, height: 100)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    
-                                    // Deskripsi Menu
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text(tenant.menuItems[index].name)
-                                            .font(.headline)
-                                        
-                                        Text(tenant.menuItems[index].des)
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                        
-                                        Text("Harga: \(tenant.menuItems[index].price)")
-                                            .font(.subheadline)
-                                            .bold()
-                                    }
-                                    Spacer()
-                                    
-                                    // Toggle Favorite
-                                    Button(action: {
-                                        let isNowFavorite = toggleFavorite(menu: tenant.menuItems[index])
-                                        alertMessage = isNowFavorite ? "Menu telah ditambahkan ke daftar favorit." : "Menu telah dihapus dari daftar favorit."
-                                        
-                                        if isNowFavorite {
-                                            showRedirectAlert = true
-                                        }
-                                        
-                                    }) {
-                                        Image(systemName: favoriteManager.isFavorite(menu: tenant.menuItems[index]) ? "star.fill" : "star")
-                                            .font(.system(size: 24))
-                                            .foregroundColor(favoriteManager.isFavorite(menu: tenant.menuItems[index]) ? .yellow : .gray)
-                                    }
-                                    .alert(isPresented: $showRedirectAlert) {
-                                        Alert(
-                                            title: Text("Menu Ditambahkan!"),
-                                            message: Text("Apakah Anda ingin melihat daftar favorit?"),
-                                            primaryButton: .default(Text("Ya"), action: {
-                                                isRedirecting = true // Mengarahkan ke FavoriteMenuView
-                                            }),
-                                            secondaryButton: .cancel(Text("Tidak"))
-                                        )
-                                    }
-                                }
-                                .frame(width: 350)
-                                .padding(.vertical, 10)
+                        
+                        // Daftar Menu
+                        if tenant.menuItems.isEmpty {
+                            Text("Tidak ada menu tersedia")
+                                .foregroundColor(.gray)
+                                .italic()
+                        } else {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Menu:")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .padding(.bottom, 5)
                                 
-                                if index < tenant.menuItems.count - 1 {
-                                    Divider()
-                                        .background(Color.gray)
-                                        .padding(.horizontal, -10)
+                                // Iterasi menu
+                                ForEach(tenant.menuItems.indices, id: \.self) { index in
+                                    HStack(spacing: 15) {
+                                        // Gambar Menu
+                                        Image(tenant.menuItems[index].image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 130, height: 100)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        
+                                        // Deskripsi Menu
+                                        VStack(alignment: .leading, spacing: 5) {
+                                            Text(tenant.menuItems[index].name)
+                                                .font(.headline)
+                                            
+                                            Text(tenant.menuItems[index].des)
+                                                .font(.subheadline)
+                                                .foregroundColor(.gray)
+                                            
+                                            Text("Harga: \(tenant.menuItems[index].price)")
+                                                .font(.subheadline)
+                                                .bold()
+                                        }
+                                        Spacer()
+                                        
+                                        // Toggle Favorite
+                                        Button(action: {
+                                            let isNowFavorite = toggleFavorite(menu: tenant.menuItems[index])
+                                            alertMessage = isNowFavorite ? "Menu telah ditambahkan ke daftar favorit." : "Menu telah dihapus dari daftar favorit."
+                                            
+                                            if isNowFavorite {
+                                                showRedirectAlert = true
+                                            }
+                                            
+                                        }) {
+                                            Image(systemName: favoriteManager.isFavorite(menu: tenant.menuItems[index]) ? "star.fill" : "star")
+                                                .font(.system(size: 24))
+                                                .foregroundColor(favoriteManager.isFavorite(menu: tenant.menuItems[index]) ? .yellow : .gray)
+                                        }
+                                        .alert(isPresented: $showRedirectAlert) {
+                                            Alert(
+                                                title: Text("Menu Ditambahkan!"),
+                                                message: Text("Apakah Anda ingin melihat daftar favorit?"),
+                                                primaryButton: .default(Text("Ya"), action: {
+                                                    isRedirecting = true // Mengarahkan ke FavoriteMenuView
+                                                }),
+                                                secondaryButton: .cancel(Text("Tidak"))
+                                            )
+                                        }
+                                    }
+                                    .frame(width: 350)
+                                    .padding(.vertical, 10)
+                                    
+                                    if index < tenant.menuItems.count - 1 {
+                                        Divider()
+                                            .background(Color.gray)
+                                            .padding(.horizontal, -10)
+                                    }
                                 }
                             }
                         }
                     }
+                    .padding()
+                    .frame(width: 380)
+                    .frame(maxWidth: .infinity)
                 }
-                .padding()
-                .frame(width: 380)
-                .frame(maxWidth: .infinity)
-            }
-            .navigationTitle("Detail Tenant")
-            .navigationDestination(isPresented: $isRedirecting) {
-                FavoriteMenuView()
+                .navigationTitle("")
+                .navigationDestination(isPresented: $isRedirecting) {
+                    FavoriteMenuView()
+                }
             }
         }
     }
-}
-
-
-class FavoriteManager: ObservableObject {
-    static let shared = FavoriteManager()
+    }
     
-    @Published private(set) var favoriteMenus: [MenuItem] = []
     
-    private init() {}
-
-    func addFavorite(menu: MenuItem) {
-        if !favoriteMenus.contains(where: { $0.name == menu.name }) {
-            favoriteMenus.append(menu)
+    class FavoriteManager: ObservableObject {
+        static let shared = FavoriteManager()
+        
+        @Published private(set) var favoriteMenus: [MenuItem] = []
+        
+        private init() {}
+        
+        func addFavorite(menu: MenuItem) {
+            if !favoriteMenus.contains(where: { $0.name == menu.name }) {
+                favoriteMenus.append(menu)
+            }
+        }
+        
+        func removeFavorite(menu: MenuItem) {
+            favoriteMenus.removeAll { $0.name == menu.name }
+        }
+        
+        func isFavorite(menu: MenuItem) -> Bool {
+            return favoriteMenus.contains(where: { $0.name == menu.name })
         }
     }
     
-    func removeFavorite(menu: MenuItem) {
-        favoriteMenus.removeAll { $0.name == menu.name }
-    }
     
-    func isFavorite(menu: MenuItem) -> Bool {
-        return favoriteMenus.contains(where: { $0.name == menu.name })
+    private func toggleFavorite(menu: MenuItem) -> Bool {
+        if FavoriteManager.shared.isFavorite(menu: menu) {
+            FavoriteManager.shared.removeFavorite(menu: menu)
+            return false
+        } else {
+            FavoriteManager.shared.addFavorite(menu: menu)
+            return true
+        }
     }
-}
 
-
-private func toggleFavorite(menu: MenuItem) -> Bool {
-    if FavoriteManager.shared.isFavorite(menu: menu) {
-        FavoriteManager.shared.removeFavorite(menu: menu)
-        return false
-    } else {
-        FavoriteManager.shared.addFavorite(menu: menu)
-        return true
-    }
-}
 
 #Preview {
     TenantDetailView(tenant: Tenant(
